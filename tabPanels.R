f03350bf936391253df13903fcbb24ab637e4545
Richard AJ Tyre <atyre2@unl.edu>
2017-02-13 14:56:06 -0600

    Adding UI elements from M2-P1

--- tabPanels.R
+++ tabPanels.R
@@ -1,4 +1,19 @@
 ## Create tabpanel objects here
 ## they should be named tP<i> where <i> is the
 ## index of the order they are created.
-tP1 <- fluidPage() # end fluidpage
+tP1 <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("tp1", "Initial population size", min  = 0, max = 20, step = 0.5, value = 15),
+      sliderInput("tp1b0", "Per capita birth rate", min = 0, max = 1, step = 0.01, value = 0.2),
+      sliderInput("tp1d0", "Per capita death rate", min = 0, max = 1, step = 0.01, value = 0.25)
+    ),
+    mainPanel(
+      fluidRow(
+        column(6,plotOutput("tp1popByTime")),
+        column(6,plotOutput("tp1ratesByPop"))
+      )
+    )
+  )
+  
+) # end fluidpage
